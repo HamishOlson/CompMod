189e2ae68448c0d28e6e4ccc2b0f06fbd00d8c4a
HamishOlson <picohamish@aol.com>
2015-10-20 14:55:39 +0100

    Update Vector3D.java

--- Vector3D.java
+++ Vector3D.java
@@ -1,1 +1,18 @@
+/**
+ *Vector 3D clas
+ *
+ *@author H.Olson
+ *@author J.Steel
+ *@author A.Whittingdon
+ *@version "Oct 2015"
+ *
+ */
 
+public class Vector3D {
+
+    private double Xvalue;
+    private double Yvalue;
+    private double Zvalue;
+
+
+    public Vector (){
